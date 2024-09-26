f6f4575202ce6c3d66e5a41a1c77e53bf3967f05
c1919 <superneonbros14@gmail.com>
2024-09-26 20:41:08 +0700

    Update TearOS.py
    some minor fixes

--- src/TearOS.py
+++ src/TearOS.py
@@ -11,8 +11,8 @@ osname = "TearOS beta 1.1"
 #the variables (important i swear)
 
 print("welcome to the login script!")
-username = input("what is your name?")
-password = input("what is your password?")
+username = input("what is your name? ")
+password = input("what is your password? ")
 
 print("welcome to " + osname + "!")
 print("type 'help' for a list of commands!")
