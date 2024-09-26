2fbe0a67309d6f8d97e39b79fd607ec5d85a9c63
c1919 <superneonbros14@gmail.com>
2024-09-26 15:52:45 +0700

    Update TearOS.py
    added new command + login script

--- src/TearOS.py
+++ src/TearOS.py
@@ -1,4 +1,4 @@
-#TearOS beta 1.0
+#TearOS beta 1.1
 
 import time
 from datetime import date
@@ -7,8 +7,12 @@ today = date.today()
 chour = time.strftime("%H:")
 cminute = time.strftime("%M:")
 csecond = time.strftime("%S ")
-osname = "TearOS beta 1.0"
+osname = "TearOS beta 1.1"
 #the variables (important i swear)
+
+print("welcome to the login script!")
+username = input("what is your name?")
+password = input("what is your password?")
 
 print("welcome to " + osname + "!")
 print("type 'help' for a list of commands!")
@@ -23,12 +27,16 @@ while True:
 		print("help - Views all the commands")
 		print("time - Tells the time")
 		print("date - Tells the current date")
+		print("sysinfo - Tells the system information")
 	
 	elif cmd=="time":
 		print(chour + cminute + csecond)
 		
 	elif cmd=="date":
 		print(str(today.day) + "." + str(today.month) + "." + str(today.year))
+
+	elif cmd=="sysinfo":
+		 print(osname + " is registered to: " + username)
 		
 		#commands after the first one must be written with an "elif"
 		#statement. the commands end at the "else" mentioned here:
