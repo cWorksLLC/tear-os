0560c8aa1633999aaf98115aac74a540c9e29b6c
Glados <ezrag2000@proton.me>
2024-09-27 18:41:53 -0700

    Update TearOS.py

--- src/TearOS.py
+++ src/TearOS.py
@@ -1,54 +1,46 @@
 #TearOS beta 1.2
 
 import time
-from datetime import date
+import datetime
 
-today = date.today()
-chour = time.strftime("%H:")
-cminute = time.strftime("%M:")
-csecond = time.strftime("%S ")
-osname = "TearOS beta 1.2"
-#the variables (important i swear)
+print("Welcome to the login script!")
+username = input("What is your name? ")
+password = input("What is your password? ")
 
-print("welcome to the login script!")
-username = input("what is your name? ")
-password = input("what is your password? ")
-
-print("welcome to " + osname + "!")
-print("type 'help' for a list of commands!")
+print("Welcome to TearOS beta 1.2!)
+print("Type 'help' for a list of commands.")
 
 while True:
-	#the command script
+	# The command script
 	print("What to run?")
 	cmd = input()
 	
-	if cmd=="help":
-		print("the commands are:")
+	if cmd == "help":
+		print("The commands are:")
 		print("help - Views all the commands")
 		print("time - Tells the time")
 		print("date - Tells the current date")
 		print("sysinfo - Tells the system information")
 		print("su - Switches to Superuser")
 	
-	elif cmd=="time":
-		print(chour + cminute + csecond)
+	elif cmd == "time":
+		print(datetime.datetime.now().strftime('%H:%M:%S'))
 		
-	elif cmd=="date":
-		print(str(today.day) + "." + str(today.month) + "." + str(today.year))
+	elif cmd == "date":
+		print(datetime.datetime.now().strftime('%Y-%m-%d'))
 
-	elif cmd=="sysinfo":
-		 print(osname + " is registered to: " + username)
+	elif cmd == "sysinfo":
+		 print(f"TearOS beta 1.2 is registered to: {username}")
 
-	elif cmd=="su":
-		supass = input("what is your password?")
+	elif cmd == "su":
+		supass = input("What is your password?")
 		if supass == password:
-			print("sucess!")
+			print("Success!")
 		else:
-			print("wrong.")
+			print("Incorrect.")
 		
-		#commands after the first one must be written with an "elif"
-		#statement. the commands end at the "else" mentioned here:
+		# Commands after the first one must be written with an "elif"
+		# Statement. the commands end at the "else" mentioned here:
 		
 	else:
-		print("not a valid command.")
-#end
+		print("Not a valid command.")
