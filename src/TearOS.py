6f1e8deb316c7529f6d2c3a0610e7ff01d88979f
T_cat9000_2 <terpstragraham@gmail.com>
2024-09-29 09:48:20 -0700

    fix bugs

--- src/TearOS.py
+++ src/TearOS.py
@@ -1,6 +1,3 @@
-#TearOS beta 1.3
-# bug fixes needed
-
 import time
 from datetime import datetime, date
 import pytz
@@ -22,7 +19,7 @@ def choose_timezone():
         except ValueError:
             print("Invalid input. Please enter a number.")
 
-#the variables (important i swear)
+# Variables
 today = date.today()
 chour = time.strftime("%H:")
 cminute = time.strftime("%M:")
@@ -30,57 +27,58 @@ csecond = time.strftime("%S ")
 osname = "TearOS beta 1.3"
 dir = "home"
 
-# setting vars
+# Setting vars
 print("welcome to the login script!")
 username = input("what is your name? ")
 password = input("what is your password? ")
+
+# Set the time zone
+user_tz = choose_timezone()
+user_tz = pytz.timezone(user_tz)
 
 print("welcome to " + osname + "!")
 print("type 'help' for a list of commands!")
 
 while True:
-	#the command script
-	cmd = input(f"TearOS/{username}/{dir}>")
-	
-	if cmd=="help":
-		print("the commands are:")
-		print("help - Views all the commands")
-		print("time - Tells the time")
-		print("date - Tells the current date")
-		print("sysinfo - Tells the system information")
-		print("su - Switches to Superuser")
-		print("changedir - params: --dirname - switches directory's")
-	
-	elif cmd == "time":
-		current_time = datetime.now(user_tz).strftime("%H:%M:%S")
-	        print(f"The current time is {current_time}")
-		
-	elif cmd=="date":
-        	print(f"{today.day}.{today.month}.{today.year}")
+    # The command script
+    cmd = input(f"TearOS/{username}/{dir}>")
 
-	elif cmd=="sysinfo":
-		 print(osname + " this software copy is registered to: " + username)
+    if cmd == "help":
+        print("the commands are:")
+        print("help - Views all the commands")
+        print("time - Tells the time")
+        print("date - Tells the current date")
+        print("sysinfo - Tells the system information")
+        print("su - Switches to Superuser")
+        print("changedir - params: --dirname - switches directory's")
 
-	elif cmd=="su":
-		supass = input("what is your password?")
-		if supass == password:
-			print("sucess!")
-			username = "root" # or anything else the username for TearOS superuser would be.
-			dir = "root"
-		else:
-			print("wrong.")
-		
-		#commands after the first one must be written with an "elif"
-		#statement. the commands end at the "else" mentioned here:
-	
-	elif cmd=="changedir":
-		print("changedir usage: changedir --dirname")
-	elif cmd=="changedir --root":
-		print("root")
-		dir = "root"
-	elif cmd=="changedir --home":
-		print("home")
-		dir = "home"
-	else:
-		print("not a valid command.")
+    elif cmd == "time":
+        current_time = datetime.now(user_tz).strftime("%H:%M:%S")
+        print(f"The current time is {current_time}")
+
+    elif cmd == "date":
+        print(f"{today.day}.{today.month}.{today.year}")
+
+    elif cmd == "sysinfo":
+        print(osname + " this software copy is registered to: " + username)
+
+    elif cmd == "su":
+        supass = input("what is your password?")
+        if supass == password:
+            print("success!")
+            username = "root"  # or anything else the username for TearOS superuser would be.
+            dir = "root"
+        else:
+            print("wrong.")
+
+    elif cmd == "changedir":
+        print("changedir usage: changedir --dirname")
+    elif cmd == "changedir --root":
+        print("root")
+        dir = "root"
+    elif cmd == "changedir --home":
+        print("home")
+        dir = "home"
+    else:
+        print("not a valid command.")
 #end
